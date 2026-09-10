8413e9861fdb7d4fc205c8b7e78c3e3e6da9d5a1
Frostre_ <n9043395@gmail.com>
2026-09-10 19:34:44 +0200

    Add EmulatorError enum for error handling

---
+++ src/error/mod.rs
@@ -0,0 +1,15 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum EmulatorError {
+    #[error("PTY error: {0}")]
+    Pty(String),
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Terminal error: {0}")]
+    Terminal(String),
+    #[error("Config error: {0}")]
+    Config(String),
+}
+
+pub type Result<T> = std::result::Result<T, EmulatorError>;
